4abd298a1e8317636a3911c830618dcba7d045ef
CODEHakR1234 <hellolee1234qwert@gmail.com>
2025-09-24 23:44:05 +0900

    insert githublink

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,10 +7,18 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const githubUrl = "https://github.com/CODEHakR1234"
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h2>
+    <div class={classNames(displayClass, "page-title-block")}>
+      <h2 class="page-title">
+        <a href={baseDir}>{title}</a>
+      </h2>
+      <div class="page-title-sub">
+        <a href={githubUrl} target="_blank" rel="me noopener noreferrer">
+          @CODEHakR1234
+        </a>
+      </div>
+    </div>
   )
 }
 
@@ -19,6 +27,15 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+}
+
+.page-title-sub {
+  margin-top: 0.25rem;
+  font-size: 0.95rem;
+}
+
+.page-title-sub a {
+  color: var(--secondary);
 }
 `
 
